36a3f6a7dddc6e4ad2119c5bf69fe1a5152b606d
ali-sharifi-23 <a.sharifi4@email.kntu.ac.ir>
2024-07-11 19:58:46 +0330

    Ali's folder added

---
+++ RL_Ali/learn_kamal.py
@@ -0,0 +1,31 @@
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.env_checker import check_env
+from kamal_env import CableControlEnv
+from callback import RenderCallback
+
+# def make_env():
+#     def _init():
+#         env = CableControlEnv(render_mode="human")
+#         return env
+#     return _init
+
+# # Create the vectorized environment
+# n_envs = 6  # Define the number of environments
+# env = make_vec_env(make_env, n_envs=n_envs)
+# Initialize your environment
+env = CableControlEnv(render_mode="human")
+
+# Check your custom environment
+check_env(env)
+
+render_callback = RenderCallback()
+
+# # Create the vectorized environment
+# env = make_vec_env(lambda: env, n_envs=6)
+# Define and train the model
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log="./ppo_cable_control_tensorboard/")
+model.learn(total_timesteps=200000, log_interval=4, callback=render_callback)
+model.save("ppo_cable_control3")
+
+# %tensorboard --logdir ./ppo_cable_control_tensorboard/ --port 6007
---
+++ RL_Ali/test_kamal.py
@@ -0,0 +1,99 @@
+# from stable_baselines3 import PPO
+# from kamal_env import CableControlEnv
+# import cv2
+# import imageio
+
+# env = CableControlEnv(render_mode="rgb_array")
+# model = PPO.load("ppo_cable_control.zip")
+
+# obs, info = env.reset()
+# frames = []
+# for _ in range(500):
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, reward, done, truncated, info = env.step(action)
+
+#     image = env.render()
+#     if _ % 5 == 0:
+#         frames.append(image)
+#     if done or truncated:
+#         obs, info = env.reset()
+
+# print('observe: ' , obs)
+# imageio.mimsave('ppo_cable_control.gif', frames, fps=120)
+
+from stable_baselines3 import PPO
+from kamal_env_ali import CableControlEnv
+import cv2
+import imageio
+
+# Initialize the environment and the model
+env = CableControlEnv(render_mode="human")
+model = PPO.load("ppo_cable_control3.zip")
+
+# Reset the environment
+obs, info = env.reset()
+frames = []
+
+# Run the model for a number of steps and collect frames
+for _ in range(2000):
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, done, truncated, info = env.step(action)
+
+    # Render the environment and store the frame
+    image = env.render()
+    frames.append(image)
+    if done or truncated:
+        obs, info = env.reset()
+
+# Save the frames as a video
+video_filename = 'ppo_cable_control5.mp4'
+height, width, layers = frames[0].shape
+video = cv2.VideoWriter(video_filename, cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+
+for frame in frames:
+    video.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+video.release()
+print(f'Video saved as {video_filename}')
+
+
+# from stable_baselines3 import PPO
+# from kamal_env1 import CableControlEnv
+# import cv2
+# import imageio
+
+# # Initialize the environment and the model
+# env = CableControlEnv(render_mode="rgb_array")
+# model = PPO.load("ppo_cable_control1.zip")
+
+# # Define new targets
+# def generate_test_targets(env, radius=0.5, center=(0, -0.03, 0.8), num_targets=6):
+#     return env._generate_circular_targets(radius, center, num_targets)
+
+# new_targets = generate_test_targets(env)
+
+# # Reset the environment with new targets
+# obs, info = env.reset(options={'new_targets': new_targets})
+# frames = []
+
+# # Run the model for a number of steps and collect frames
+# for _ in range(500):
+#     action, _states = model.predict(obs, deterministic=True)
+#     obs, reward, done, truncated, info = env.step(action)
+
+#     # Render the environment and store the frame
+#     image = env.render()
+#     frames.append(image)
+#     if done or truncated:
+#         obs, info = env.reset(options={'new_targets': new_targets})
+
+# # Save the frames as a video
+# video_filename = 'ppo_cable_control5.mp4'
+# height, width, layers = frames[0].shape
+# video = cv2.VideoWriter(video_filename, cv2.VideoWriter_fourcc(*'mp4v'), 30, (width, height))
+
+# for frame in frames:
+#     video.write(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+
+# video.release()
+# print(f'Video saved as {video_filename}')

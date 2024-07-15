31929d8ef53302e4ef461929c5c5950fec9566f1
ali-sharifi-23 <a.sharifi4@email.kntu.ac.ir>
2024-07-15 12:38:16 +0330

    circle

--- RL_Ali/circle_trajectory.py
+++ RL_Ali/circle_trajectory.py
@@ -5,6 +5,7 @@ C = [0, 1.15]
 r = 0.2
 frame_skip = 5
 T = np.linspace(0,0.002*frame_skip,10)
+print(T)
 phi = np.zeros(len(T)+1)
 x = np.zeros(len(T))
 y = np.zeros(len(T))
@@ -12,7 +13,7 @@ for i in range(len(T)):
     x[i] = C[0] + r*np.sin(phi[i])
     y[i] = C[1] + r*np.cos(phi[i])
     phi[i+1] = phi[i] + 0.7*(1-np.tanh(0.6*T[i]))
-    print(f"{x[i]} -0.03 {y[i]}")
+    # print(f"{x[i]} -0.03 {y[i]}")
 
 plt.figure()
 plt.plot(x,y)

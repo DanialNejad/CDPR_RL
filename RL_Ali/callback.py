85b704066d58e2b5729cab091b2cecc3b244b3e0
DanialNejad <danialabdollahinejad@gmail.com>
2024-10-31 11:29:10 +0330

    env updated

--- RL_Ali/callback.py
+++
@@ -1,9 +0,0 @@
-from stable_baselines3.common.callbacks import BaseCallback
-
-class RenderCallback(BaseCallback):
-    def __init__(self, verbose=0):
-        super(RenderCallback, self).__init__(verbose)
-
-    def _on_step(self) -> bool:
-        self.training_env.render()
-        return True
